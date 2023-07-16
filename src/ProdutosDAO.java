5dc966621d9cf6dc0247d857cd7cb24de12f1604
Andre Lohr <andrefelipelohr@gmail.com>
2023-07-16 19:47:05 -0300

    Método Vender produto

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -58,11 +58,49 @@ public class ProdutosDAO {
         } catch (Exception e) {
             System.out.println("Erro ao listar produtos: " + e.getMessage());
         }
-
         return listagem;
     }
     
-    
+    public void venderProduto(int id) {
+    String sql = "UPDATE produtos SET status = ? WHERE id = ?";
+    conn = new conectaDAO().connectDB();
+    int aVenda = aVenda(id);
+    int vendido;
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setString(1, "Vendido");
+        prep.setInt(2, id);
+        vendido = prep.executeUpdate();
+        if (vendido == 1 && aVenda == 1) {
+            JOptionPane.showMessageDialog(null, "Venda do produto realizada com sucesso.");
+        } else if (vendido == 1 && aVenda == 0) {
+            JOptionPane.showMessageDialog(null, "Venda não realizada!\nO produto informado já está vendido.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Produto não encontrado!\nNão há produtos cadastrados no ID informado.");
+        }
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Erro.\nErro durante a execução da função Vender.");
+    }
+}
+
+public int aVenda(int id) {
+    String sql = "SELECT COUNT(*) FROM produtos WHERE id = ? AND status = ?";
+    conn = new conectaDAO().connectDB();
+
+    try {
+        prep = conn.prepareStatement(sql);
+        prep.setInt(1, id);
+        prep.setString(2, "A Venda");
+        resultset = prep.executeQuery();
+        resultset.next();
+        return resultset.getInt("COUNT(*)");
+
+    } catch (Exception e) {
+        return 0;
+    }
+}
+
     
         
 }
